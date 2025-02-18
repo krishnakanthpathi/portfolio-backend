f462f9ac4260f61d4e4dc80db7e26cae0e79afc9
krishnakanth <krishnakanthpathi@gmail.com>
2025-02-18 21:08:29 +0530

    completion of localdb and get request

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ import achievementsRouter from "./routes/achievements.route.js";
 import projectsRouter from "./routes/projects.route.js";
 import connnectDB from "./config/mongodb.config.js";
 
+// models
+import Project from "./models/projects.model.js";
+import Achievements from "./models/achievements.model.js";
+import Resume from "./models/resume.model.js";
+import About from "./models/about.model.js";
+
 const app = express();
 const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 dotenv.config();
 connnectDB();
 
@@ -24,6 +30,42 @@ app.get("/" , (req , res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/projects", async (req, res) => {
+    try {
+        const projects = await Project.find({}); 
+        res.json(projects);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get("/achievements" , async (req , res) => {
+    try {
+        const achievements = await Achievements.find({});
+        res.json(achievements);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get("/about" , async (req , res) => {
+    try {
+        const about = await About.find({});
+        res.json(about);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get("/resume" ,async (req , res) => {
+    try {
+        const resume = await Resume.find({});
+        res.json(resume);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+// post routes
 
 app.use("/api/projects" , projectsRouter    );
 app.use("/api/achievements" , achievementsRouter);
